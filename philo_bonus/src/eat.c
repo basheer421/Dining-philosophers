edf14c7a13d1a13f1b90e770139d28e3de790bbf
basheer421 <basheer123b2@gmail.com>
2023-03-18 23:04:25 +0400

    not tested

---
+++ philo_bonus/src/eat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   eat.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/18 23:01:13 by bammar            #+#    #+#             */
+/*   Updated: 2023/03/18 23:01:31 by bammar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+bool	eat(t_process_arg *targ)
+{
+	if (dead(targ) || targ->args->count == 1)
+		return (false);
+	sem_wait(targ->forks_sem);
+	targ->philo->state = EATING;
+	if (targ->args->is_limited
+		&& targ->args->eat_limit == targ->philo->eat_count)
+		return (true);
+	print_msg(targ, "has taken a fork");
+	print_msg(targ, "has taken a fork");
+	print_msg(targ, "is eating");
+	targ->philo->last_mealtime = get_time();
+	psleep(targ->args->eat_time);
+	targ->philo->eat_count++;
+	sem_post(targ->forks_sem);
+	return (true);
+}
--- philo_bonus/src/life.c
+++ philo_bonus/src/life.c
@@ -6,11 +6,36 @@
 /*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 22:14:59 by bammar            #+#    #+#             */
-/*   Updated: 2023/03/18 22:49:38 by bammar           ###   ########.fr       */
+/*   Updated: 2023/03/18 23:04:05 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	think(t_process_arg *targ)
+{
+	if (dead(targ) || targ->philo->state == THINKING)
+		return ;
+	print_msg(targ, "is thinking");
+	targ->philo->state = THINKING;
+}
+
+void	go_sleep(t_process_arg *targ)
+{
+	if (dead(targ) || targ->philo->state == SLEEPING)
+		return ;
+	print_msg(targ, "is sleeping");
+	psleep(targ->args->sleep_time);
+	targ->philo->state = SLEEPING;
+}
+
+static void	increase_limits_reached(t_process_arg *targ)
+{
+	sem_wait(targ->limit_sem);
+	if (targ->philo->eat_count == targ->args->eat_limit)
+		(*targ->limits_reached)++;
+	sem_post(targ->limit_sem);
+}
 
 philo_life(t_process_arg *targ)
 {
